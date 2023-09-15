083a1d041c8331a3abb1ba7740d8137d3c1854a7
Alan Vicente <86435289+perfildoalan@users.noreply.github.com>
2023-09-14 21:29:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 function copiar() {
   
-// Get the text field
-var copyText = document.getElementById("password");
+    // Get the text field
+    var copyText = document.getElementById("password");
 
-// Select the text field
-copyText.select();
-copyText.setSelectionRange(0, 99999); // For mobile devices
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
 
- // Copy the text inside the text field
-navigator.clipboard.writeText(copyText.value);
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
 
-// Alert the copied text
-alert("Senha " + copyText.value + " copiada");
+    // Alert the copied text
+    toastOn("Senha " + copyText.value + " copiada");
+    setTimeout (() => {toastOff("Senha " + copyText.value + " copiada")}, 2000);
 
+}
+
+function toastOn(value) {
+    var toastOn = document.getElementById("toast-off")
+    toastOn.textContent = value;
+    toastOn.id = "toast-on";
+}
+
+function toastOff(value) {
+    var toastOff = document.getElementById("toast-on")
+    toastOff.textContent = value;
+    toastOff.id = "toast-off";
 }
