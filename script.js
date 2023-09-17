5057ee8dffb48f366c5ae11f76dc81cee2564c16
Alan Vicente <86435289+perfildoalan@users.noreply.github.com>
2023-09-16 21:23:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-function aparecerCadastro() {
-    var cadastro = document.getElementById("pop-cadastro-null");
-    cadastro.id = "pop-cadastro";
+function copiar() {
+  
+    // Get the text field
+    var copyText = document.getElementById("password");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    toastOn("Senha " + copyText.value + " copiada");
+    setTimeout (() => {toastOff("Senha " + copyText.value + " copiada")}, 2000);
+
 }
 
-function desaparecerCadastro() {
-    var cadastro = document.getElementById("pop-cadastro");
-    cadastro.id = "pop-cadastro-null";
+function toastOn(value) {
+    var toastOn = document.getElementById("toast-off")
+    toastOn.textContent = value;
+    toastOn.id = "toast-on";
 }
+
+function toastOff(value) {
+    var toastOff = document.getElementById("toast-on")
+    toastOff.textContent = value;
+    toastOff.id = "toast-off";
+}
+
+document.getElementById("SSID").addEventListener("mousedown", function(event){
+    event.preventDefault();
+  });
